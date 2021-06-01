6a2e2ed89975a602c98d88e328c3f83b91c60c79
Josua Michalak <michalak.josua@gmail.com>
2021-06-01 20:37:45 +0200

    added render method

--- src/Post/PostsController.php
+++ src/Post/PostsController.php
@@ -10,11 +10,19 @@ class PostsController
         $this->postsRepository = $postsRepository;
     }
 
+    protected function render($view, $params) 
+    {
+        extract($params);
+        include __DIR__ . "/../../views/{$view}.php";
+    }
+
     public function index() 
     {
         $posts = $this->postsRepository->fetchPosts();
 
-        include __DIR__ . "/../../views/post/index.php";
+        $this->render("post/index", [
+            'posts' => $posts
+        ]);
     }
 
     public function show() 
@@ -22,13 +30,18 @@ class PostsController
         $id = $_GET['id'];
         $post = $this->postsRepository->fetchPost($id);
 
-        include __DIR__ . "/../../views/post/post.php";
+        $this->render("post/post", [
+            'id' => $id,
+            'post' => $post
+        ]);
     }
 
     public function sitemap()
     {
         $posts = $this->postsRepository->fetchPosts();
 
-        include __DIR__ . "/../../views/post/sitemap.php";
+        $this->render("post/sitemap", [
+            'posts' => $posts
+        ]);
     }
 }
