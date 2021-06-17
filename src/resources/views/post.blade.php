5b3f837426ee53d714ffb2cda57412aaaa00a6ed
Josua Michalak <michalak.josua@googlemail.com>
2021-06-17 16:09:01 +0200

    added blade syntax to views

--- src/resources/views/post.blade.php
+++ src/resources/views/post.blade.php
@@ -8,25 +8,25 @@
     <div class="panel panel-default">
         <div class="panel-heading">
             <h3 class="panel-title">
-                <?php echo("[{$post->getId()->value()}] " . $post->getTitle()->value()); ?>
+                [{{$post->getId()->value()}}] {{$post->getTitle()->value()}}
             </h3>
         </div>
         <div class="panel-body">
             <p>
-                <?php echo nl2br($post->getContent()->value()); ?>
+                {!! nl2br($post->getContent()->value()) !!}
             </p>
         </div>
     </div>
 
     <ul class="list-group">
-        <?php foreach($comments AS $comment): ?>
+        @foreach($comments as $comment)
         <li class="list-group-item">
-            <?php echo $comment->getContent()->value(); ?>
+            {{$comment->getContent()->value()}}
         </li>
-        <?php endforeach; ?>
+        @endforeach
     </ul>
 
-    <form action="post-<?php echo $post->getId()->value();?>" method="post">
+    <form action="post-{{$post->getId()->value()}}" method="post">
         <textarea name="content" class="form-control" style="resize:none; margin-bottom:2rem;"></textarea>
         <input type="submit" value="Add comment" class="btn btn-primary">
     </form>
